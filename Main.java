a1c9e878ecea32d64901370691c2cd67060897b7
LongTaco <88407126+LongTaco@users.noreply.github.com>
2024-11-04 14:11:00 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,179 @@
+import java.util.*;
+
+public class Main
+{
+    static Random randy = new Random();
+    static int shuffles = 0;
+    static int max = 101;
+    static int len = 10;
+
+    public static void main(String[] args)
+    {
+        int[] array = new int[len];
+        Scanner scanner = new Scanner(System.in);
+
+        while (true)
+        {
+            System.out.print("> ");
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("exit"))
+            {
+                break;
+            }
+            String[] parts = input.split(" ");
+            String command = parts[0];
+
+            try
+            {
+                switch(command)
+                {
+                    case "sort":
+                    System.out.println("Which method would you like to use to sort?");
+                    String method = scanner.nextLine();
+                    switch(method)
+                    {
+                        case "selection":
+                        System.out.println("Which array would you like to sort?");
+                         
+                        case "random":
+
+                    }
+
+                    case "search":
+                    System.out.println("What would you like to search for?");
+
+                    case "print":
+                }
+            }
+            catch (Exception e)
+            {
+                System.out.println("Please enter a valid command.");
+            }
+
+
+
+        }
+        scanner.close();
+    }
+
+    public static void fillArray(int[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            array[i] = randy.nextInt(max);
+        }
+    }
+
+    public static void printArray(int[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.print(array[i]);
+            System.out.print(" ");
+        }
+        System.out.print("\n");
+    }
+
+    public static void searchArray(int [] array, int x)
+    {
+        if (isSorted(array))
+        {
+            binarySearch(array, x);
+        }
+        else
+        {
+            selectionSort(array);
+            binarySearch(array, x);
+        }
+    }
+
+    public static void binarySearch(int[] array, int x)
+    {
+        boolean found = false;
+        int first = 0;
+        int last = array.length - 1;
+        int middle = (first + last)/2;
+        while (first <= last && !found)
+        {
+            middle = (first + last)/2;
+            if (array[middle] == x)
+            {
+                found = true;
+                System.out.println("Found at index: " + middle);
+            }
+
+            else if (x > array[middle])
+            {
+                first = middle + 1;
+            }
+
+            else if (x < array[middle])
+            {
+                last = middle - 1;
+            }
+        }
+
+        if (!found)
+        {
+            System.out.println("Not in array");
+        }
+    }
+
+    public static boolean isSorted(int[] array)
+    {
+        for (int i = 0; i < array.length - 1;)
+        {
+            if (array[i] <= array[i + 1])
+            {
+                i++;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void shuffleArray(int[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            int j = randy.nextInt(array.length - 1);
+            int temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+            shuffles++;
+        }
+    }
+
+    public static void selectionSort(int[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            int smallest = array[i];
+            int smallestIndex = i;
+            for (int j = i + 1; j < array.length; j++)
+            {
+                if (array[j] < smallest)
+                {
+                    smallest = array[j];
+                    smallestIndex = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = smallest;
+            array[smallestIndex] = temp;
+        }
+    }
+
+    public static void randomSort(int[] array)
+    {
+        while (!isSorted(array))
+        {
+            shuffleArray(array);
+            // System.out.print("\rShuffles: " + shuffles);
+        }
+        System.out.print("\r");
+    }
+}
